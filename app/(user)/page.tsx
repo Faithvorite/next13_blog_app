e957969b9db2465d3ea0804cb2c0972d0787935e
Faithvorite <57932015+Faithvorite@users.noreply.github.com>
2022-12-21 20:09:07 -0600

    preview, type

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,7 +1,34 @@
-export default function HomePage() {
-    return (
-        <div>
+import { previewData } from "next/headers";
+import { groq } from "next-sanity";
+import { client } from "../../lib/sanity.client";
+import  PreviewSuspense from "../../components/PreviewSuspense"
+import PreviewBlogList from "../../components/PreviewBlogList";
+import BlogList from "../../components/BlogList";
+const query = groq`
+*[_type=='post'] {
+    ..., 
+    author->,
+    categories[]->
+} | order(_createdAt desc)
+`
+export default async function HomePage() {
 
-        </div>
+    if(!previewData()) {
+        return (
+        <PreviewSuspense fallback={(
+            <div role="status">
+                <p className="text-center text-lg animate-pulse text-[#F7AB0A">
+                    Loading preview data...
+                    </p>
+            </div>
+        )}
+        >
+            <PreviewBlogList query={query}/>
+        </PreviewSuspense>
+        )
+    }
+    const posts = await client.fetch(query);
+    return (
+   <BlogList posts={posts}/>
     )
 }
