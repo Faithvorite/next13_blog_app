3fdb5cad146267cbc7c50451e75158b13aec3d1a
Faithvorite <57932015+Faithvorite@users.noreply.github.com>
2022-12-29 13:32:54 -0600

    working draft

--- app/(user)/post/[slug]/page.tsx
+++ app/(user)/post/[slug]/page.tsx
@@ -11,6 +11,21 @@ type Props = {
     };
 };
 
+export const revalidate = 60;
+
+export async function generateStaticParams() {
+    const query = groq`*[_type=='post']
+    {
+        slug
+    }`;
+
+    const slugs: Post[] = await client.fetch(query);
+    const slugRoutes = slugs.map((slug) => slug.slug.current);
+
+    return slugRoutes.map((slug) => ({
+        slug: slug
+    }));
+}
 async function Post({params: { slug } }: Props) {
   const query = groq`
   *[_type=='post' && slug.current == $slug][0]
