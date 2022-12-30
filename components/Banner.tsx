f267fb5bb45566c2507904e463b6a1aaf8258876
Faithvorite <57932015+Faithvorite@users.noreply.github.com>
2022-12-30 12:41:15 -0600

    deploy

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,7 +3,7 @@ function Banner() {
   return (
   <div className="flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10">
     <div>
-        <h1 className="text-7xl">FAITH NGUYEN Blog</h1>
+        <h1 className="text-7xl">FAITH NGUYEN</h1>
         <h2 className="mt-5 md:mt-0">
             Welcom  to{" "}
             <span className="underline decoration-4 decoration-[#F7AB0A]">
