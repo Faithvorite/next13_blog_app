900da395790c94aa689af50ad42e264161c4785d
Faithvorite <57932015+Faithvorite@users.noreply.github.com>
2022-12-30 12:33:35 -0600

    id

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,9 +1,11 @@
 import {defineCliConfig} from 'sanity/cli'
 
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID! as string;
+const dataset = process.env.NEXT_PUBLIC_SANTIY_DATABASE! as string;
 
 export default defineCliConfig({
   api: {
-    projectId: 'rp9rwe05',
-    dataset: 'production'
+    projectId,
+    dataset
   }
 })
